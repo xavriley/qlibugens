23a8aea0c95ea77e1d19dcc4735c2415014ea8be
Xavier Riley <xavriley@hotmail.com>
2021-10-24 21:40:02 +0100

    First working version
    This now runs as an audio rate plugin like so:
    
    ```
    (
    s.waitForBoot({
    	arg rate = 1;
    	b = Buffer.read(s, "/Users/xavriley/Downloads/bobby.wav");
    	SinOsc.ar(BitstreamAutocorrelation.ar(PlayBuf.ar(1, b, rate, loop:1)));
    }.play, 1);
    )
    ```
    
    Key points in fixing it were to ensure that anti-aliasing was disabled
    (which makes sense as we don't output an audio signal betwen -1 and 1)
    and to ensure that a value was being written to every index of the
    output buffer even if a new pitch wasn't detected.
    
    Still work to do on removing `make_shared`

--- plugins/BitstreamAutocorrelation/BitstreamAutocorrelation.cpp
+++ plugins/BitstreamAutocorrelation/BitstreamAutocorrelation.cpp
@@ -1,5 +1,5 @@
 // PluginBitstreamAutocorrelation.cpp
-// Xavier Riley (xavriley@hotmail.com)
+// Xavier Riley (@xavriley)
 #include "SC_PlugIn.hpp"
 #include "BitstreamAutocorrelation.hpp"
 
@@ -25,25 +25,18 @@ BitstreamAutocorrelation::BitstreamAutocorrelation() :
 void BitstreamAutocorrelation::next(int nSamples) {
     auto unit = this;
     const float* input = in(0);
-    float* outbuf = ZOUT(0);
-//    float* freq = ZOUT(0);
-//    float* predicted = ZOUT(1);
-//    float* periodicity = ZOUT(2);
+    float* outbuf = out(0);
 
-    /* Period Detection */
-    auto nAudioSamples = inBufferSize(0);
-
-    for (int i = 0; i < nAudioSamples; ++i) {
+    for(int i = 0; i < nSamples; i++) {
         float s = m_pp->operator()(input[i]);
-	    bool is_ready = m_pd->operator()(s);
+        bool is_ready = m_pd->operator()(s);
 
-	    if (is_ready) {
-		    m_frequency = m_pd->get_frequency();
-            // Print("freq: %f\n", m_frequency);
-            ZXP(outbuf) = m_frequency;
-	    }
+        if (is_ready) {
+            outbuf[i] = m_frequency = m_pd->get_frequency();
+        } else {
+            outbuf[i] = m_frequency;
+        }
     }
-
 }
 
 } // namespace QlibUGens
